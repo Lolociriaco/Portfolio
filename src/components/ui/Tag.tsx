b01814f1947f394092df4cd2a2db31b8b1f1487b
Lorenzo Ciriaco <ciriacolorenzo5@gmail.com>
2025-04-14 17:46:12 -0300

    Solving tech.icon problem

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,7 +1,8 @@
 import { mont } from "@/lib/fonts"
+import React, { JSX } from "react";
 
 interface Props {
-    children: string;
+    children: React.ReactNode;
     className?: string;
 }
 
@@ -10,7 +11,7 @@ export const Tag = ( { children, className }:Props ) => {
   return (
     <>
         <span className={`px-7 py-2 mt-4 border text-gray-300 border-gray-700 focus:outline-none  focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-xs bg-red-600  dark:focus:ring-gray-700 ${mont.className} ${className} bg-opacity-20`}>
-            {children}
+            { children }
         </span>
     </>
   )
